eff18fd2086987f4e03cbb55033ade3b7d54936b
utkarsh <usrivastava92@gmail.com>
2022-05-29 15:14:58 +0530

    added .prettierrc

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,34 @@
-import './App.scss'
-import {Button, createTheme, CssBaseline, ThemeProvider, useMediaQuery} from "@mui/material";
-import React from "react";
-import {getDesignTokens} from "./config/theme";
+import './App.scss';
+import {
+  Button,
+  createTheme,
+  CssBaseline,
+  ThemeProvider,
+  Typography,
+  useMediaQuery
+} from '@mui/material';
+import React from 'react';
+import { getPaletteByMode } from './config/theme';
 
 function App() {
+  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
 
-    const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+  const theme = React.useMemo(
+    () =>
+      createTheme({
+        palette: getPaletteByMode(prefersDarkMode ? 'dark' : 'light')
+      }),
+    [prefersDarkMode]
+  );
 
-    const theme = React.useMemo(() =>
-            createTheme({
-                palette: getDesignTokens(prefersDarkMode ? 'dark' : 'light'),
-            }),
-        [prefersDarkMode],
-    );
-
-    return (
-        <ThemeProvider theme={theme}>
-            <CssBaseline/>
-            <Button color={'primary'} >Hello World</Button>
-        </ThemeProvider>
-    );
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Typography>
+        <Button color={'primary'}>Hello World</Button>
+      </Typography>
+    </ThemeProvider>
+  );
 }
 
-export default App
+export default App;
--- src/config/theme.ts
+++ src/config/theme.ts
@@ -1,25 +1,29 @@
-import {PaletteMode, PaletteOptions} from "@mui/material";
+import { PaletteMode, PaletteOptions } from '@mui/material';
 
 const lightModePallet: PaletteOptions = {
-    mode: 'light',
-    background: {
-        default: '#1C3FAA',
-    },
-    text: {
-        primary:  '#fff'
-    }
+  mode: 'light',
+  primary: {
+    main: '#1C3FAA',
+    contrastText: '#fff'
+  },
+  background: {
+    default: '#1C3FAA'
+  },
+  text: {
+    primary: '#fff'
+  }
 };
 
 const darkModePallet: PaletteOptions = {
-    mode: 'light',
-    background: {
-        default: '#232a3b',
-    },
-    text: {
-       primary:  '#fff'
-    }
+  mode: 'light',
+  background: {
+    default: '#232a3b'
+  },
+  text: {
+    primary: '#fff'
+  }
 };
 
-export const getDesignTokens = (mode: PaletteMode) => {
-    return mode === 'light' ? {...lightModePallet} : {...darkModePallet};
+export const getPaletteByMode = (mode: PaletteMode) => {
+  return mode === 'light' ? { ...lightModePallet } : { ...darkModePallet };
 };
